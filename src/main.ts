731c8b535acecfc6fea97ddbad25fd8f403e031e
Jores Amancio <amanciojoris23@gmail.com>
2022-07-05 10:33:23 +0800

    heroku test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -8,6 +9,6 @@ async function bootstrap() {
     credentials: true,
   })
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
